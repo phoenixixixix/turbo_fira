6f824d7ac7b3c3d88929b77d63b60ed5357f64d0
phoenixixixix <barchukoleg12@gmail.com>
2023-10-05 17:42:47 +0300

    Create Sign Up pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
+
   resources :stacks do
     resources :tasks, except: %i[ show ], shallow: true
   end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe "/users", type: :request do
+  let(:invalid_attributes) { { email: "abc", password: "", password_confirmation: "nomatch" } }
+
+  describe "GET /sign_up" do
+    it "renders a successful responce" do
+      get sign_up_path
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST /sign_up" do
+    context "with valid parameters" do
+      let(:valid_attributes) { { email: "post_create@mail.com", password: "welcome", password_confirmation: "welcome" } }
+
+      it "creates a new User" do
+        expect {
+          post sign_up_path, params: { user: valid_attributes }
+        }.to change { User.count }.by(1)
+      end
+
+      it "redirects to root path" do
+        post sign_up_path, params: { user: valid_attributes }
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "with invalid parameters" do
+      it "doesn't create a new User" do
+        expect {
+          post sign_up_path, params: { user: invalid_attributes }
+        }.to change { User.count }.by(0)
+      end
+
+      it "renders a response with 422 status (i.e. to display the 'new' template)" do
+        post sign_up_path, params: { user: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+end
