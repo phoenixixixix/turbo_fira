e86a5da1121228420b62f3d5e6fccfd1484c966d
phoenixixixix <barchukoleg12@gmail.com>
2023-10-08 23:07:55 +0300

    Set user_id into session on sign up

--- spec/requests/application_controller_spec.rb
+++ spec/requests/application_controller_spec.rb
@@ -2,7 +2,16 @@ require "rails_helper"
 
 RSpec.describe "ApplicationControllers", type: :request do
   describe "#current_user" do
-    it "displays user email when a current user present (i.e. user logged in)" do
+    it "expects current_user to be present after signing up" do
+      user_attributes = { email: "current-user@mail.com", password: "welcome", password_confirmation: "welcome" }
+
+      post sign_up_path, params: { user: user_attributes }
+      follow_redirect!
+
+      expect(response.body).to include(user_attributes[:email])
+    end
+
+    it "expects current_user to be present when user logged in" do
       user = create(:user)
       log_in(user)
 
